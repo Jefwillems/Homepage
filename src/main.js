fea58b062b3f744dadea27ada26b73fbac2c554a
Jefwillems <jefwillems100@gmail.com>
2019-01-10 11:49:45 +0100

    enable tree shaking

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify';
+import Vuetify from 'vuetify/lib';
 import App from './App.vue';
 import store from './store';
 import 'vuetify/src/stylus/app.styl';
