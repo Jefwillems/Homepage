44a5f8ed8e625262be461bb8580357b919402c9b
Jefwillems <jefwillems100@gmail.com>
2020-02-05 14:06:03 +0100

    apply eslint fixes

--- src/store/modules/bookmarks/getters.js
+++ src/store/modules/bookmarks/getters.js
@@ -1,13 +1,13 @@
 export default {
-  categories: ({ categories }) => categories.map(c => c.name),
+  categories: ({ categories }) => categories.map((c) => c.name),
   icons: ({ categories, selected_category: selected }) => {
     if (selected.toLowerCase() === 'any') {
-      return categories.flatMap(c => c.icons).map(c => c.id);
+      return categories.flatMap((c) => c.icons).map((c) => c.id);
     }
     return categories
-      .filter(c => c.name === selected)
-      .flatMap(c => c.icons)
-      .map(c => c.id);
+      .filter((c) => c.name === selected)
+      .flatMap((c) => c.icons)
+      .map((c) => c.id);
   },
   bookmarks: ({ bookmarks }) => bookmarks,
 };
